e493bc0f929d5a22a3184aef671e8246a324c445
Anna <ancha.ru@mail.ru>
2022-09-29 07:57:12 +0300

    второе задание из дз4

---
+++ Task4_2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+Ограничения:
+- Должна быть отдельная функция, которая возвразает результат суммы цифр
+- Внутри функций НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода*/
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine()!);
+int lenght = NumberLen(num);
+int NumberLen(int num)// длина числа
+{
+    int index = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        index++;
+    }
+    return index;
+}
+int Sum(int num, int lenght)// расчет суммы
+{
+    int sum = 0;
+    for (int i = 1; i <= lenght; i++)
+    {
+         sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+int res = Sum(num, lenght);
+Console.WriteLine($"Cумма {res}");
---
+++ Task4_3/Program.cs
@@ -0,0 +1,30 @@
+﻿/*  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+Ограничения:
+- Должна быть отдельная функция, которая создаёт массив и отдельная функция вывода
+- Внутри функций (кроме функции вывода) НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода 
+или в функции вывода*/
+Console.Clear();
+ 
+int arrayLenght = 8;
+int[] array = new int[arrayLenght];
+ 
+FillArray(array);
+PrintArray(array);
+  
+void FillArray(int[] array)
+{
+    Random randGenerator = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = randGenerator.Next();
+    }
+}
+ void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
