34b6e46f5c0b294ebae9fd5febca7dff92d9cbb0
Anna <ancha.ru@mail.ru>
2022-10-07 22:25:16 +0300

    Задание 7_3

---
+++ Task7_3/Program.cs
@@ -0,0 +1,54 @@
+﻿///Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+Arifmeans(array);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+            // Console.Write(" | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void FillArray(int[,] array)
+{
+    Random generator = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = generator.Next(0, 10);
+        }
+    }
+}
+
+void Arifmeans(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            // Console.Write(" | ");
+        }
+        Console.Write($"{j + 1} столбец: {sum / array.GetLength(0)} |");
+    }
+}
