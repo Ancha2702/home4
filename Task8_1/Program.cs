ce1b55aad51c67a5fa6b9a6f5f6c8661ecb08f69
Anna <ancha.ru@mail.ru>
2022-10-12 19:47:14 +0300

    Первое задание из 8 дз

---
+++ Task8_1/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задайте двумерный массив. Напишите программу,
+ которая упорядочит по убыванию элементы каждой строки двумерного массива.*/
+ Console.Write("Введите rколичество строк: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("Массив после сортировки по убыванию: ");
+SortArr(array);
+PrintArray(array);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+            // Console.Write(" | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    Random generator = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = generator.Next(0, 10);
+        }
+    }
+}
+
+void SortArr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
