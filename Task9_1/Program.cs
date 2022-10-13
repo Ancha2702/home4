1053a010c5c37750ab283c1095a173f8bf7f017c
Anna <ancha.ru@mail.ru>
2022-10-13 21:17:26 +0300

    Первое задание из 9 дз

---
+++ Task9_1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+ Выполнить с помощью рекурсии.*/
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine()!);
+//int numbers = NatNum(num);
+//Console.WriteLine(numbers);
+NatNum(num);
+void NatNum(int num)
+{
+    Console.Write($" {num} ");
+    if (num == 1)
+    {
+        return;
+
+    }
+    else
+    {
+
+        NatNum(num - 1);
+
+    }
+}
